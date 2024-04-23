d24252ff856f204ba93337eef09f867f9e1a0a31
Kellan Cartledge <kccartle@amazon.com>
2024-04-23 14:16:57 -0700

    feat(deploy): delete command added to deploy tool

--- packages/deploy/bin/index.js
+++ packages/deploy/bin/index.js
@@ -5,6 +5,19 @@ import checkbox from "@inquirer/checkbox";
 import input from "@inquirer/input";
 import select from "@inquirer/select";
 
+import {
+  S3Client,
+  DeleteBucketCommand,
+  DeleteObjectsCommand,
+  ListObjectsV2Command,
+} from "@aws-sdk/client-s3";
+import { SSMClient, DeleteParametersCommand } from "@aws-sdk/client-ssm";
+import {
+  EC2Client,
+  DescribeKeyPairsCommand,
+  DeleteKeyPairCommand,
+} from "@aws-sdk/client-ec2";
+
 import { execSync } from "node:child_process";
 import path from "path";
 import fs from "fs";
@@ -93,21 +106,57 @@ Usage
       deploy: {
         type: "string",
         shortFlag: "d",
-        isMultiple: true,
+        isMultiple: false,
       },
       config: {
         type: "string",
+      },
+      delete: {
+        type: "string",
+        shortFlag: "x",
+        isMultiple: false,
       },
     },
   },
 );
 
 /**
+ * Prompt user to verify Golden AMI ID
+ */
+const verifyAmiId = async () => {
+  const goldenAMIDeployed = await select({
+    message: `You have selected Omniverse Workstation Fleet for deployment. Have you already deployed the Workstation AMI?`,
+    choices: [
+      {
+        name: "Yes",
+        value: true,
+      },
+      {
+        name: "No / Not sure",
+        value: false,
+      },
+    ],
+  });
+
+  if (!goldenAMIDeployed) {
+    console.log(
+      chalk.red(`
+    Before deploying the Omniverse Workstations, it is necessary to deploy the Omniverse Workstations AMI. 
+    Please restart the Deployment Tool and select only the Omniverse Workstation AMI option.
+    The process for deploying the Omniverse Workstation AMI can take several minutes. 
+    For the additional steps post deployment, please refer to the Workstation AMI README: ${getWorkstationsAMIReadme()}
+    `),
+    );
+    process.exit(1);
+  }
+};
+
+/**
  * Prompt user to verify staged modules
- * @param {string[]} modulesToDeploy
+ * @param {string[]} modules
  * @returns
  */
-const verifyDeployment = async (modulesToDeploy) => {
+const verifyDeployment = async (modules) => {
   return await select({
     message: chalk.bold(
       chalk.cyan(
@@ -118,7 +167,7 @@ const verifyDeployment = async (modulesToDeploy) => {
   
         If your environment was not bootstrapped, we will do that for you now.
         We will be deploying the following modules:
-        ${modulesToDeploy.join(", ")}
+        ${modules.join(", ")}
   
         Proceed?
         `,
@@ -134,9 +183,9 @@ const verifyDeployment = async (modulesToDeploy) => {
 /**
  * Runs the CDK deployment process
  * @param {boolean} cdkConfigured
- * @param {string[]} modulesToDeploy
+ * @param {string[]} modules
  */
-const runDeployment = async (cdkConfigured, modulesToDeploy) => {
+const runDeployment = async (cdkConfigured, modules) => {
   if (!cdkConfigured) {
     execSync("cdk bootstrap", {
       stdio: "inherit",
@@ -153,7 +202,7 @@ const runDeployment = async (cdkConfigured, modulesToDeploy) => {
     stdio: "inherit",
   });
 
-  const stacks = modulesToDeploy.join(" ");
+  const stacks = modules.join(" ");
   execSync(`cdk deploy -c stacks="${stacks}" --all --verbose`, {
     cwd: getModuleDirectory(),
     stdio: "inherit",
@@ -163,8 +212,8 @@ const runDeployment = async (cdkConfigured, modulesToDeploy) => {
 /**
  * Iterates the module requirements prompting user with data input fields
  */
-const runModuleConfiguration = async (modulesToDeploy) => {
-  for (const module of modulesToDeploy) {
+const runModuleConfiguration = async (modules) => {
+  for (const module of modules) {
     const key = templateInverseLookup[module];
     const template = configurationTemplates[key];
     appConfig.stacks[key] = await template.config();
@@ -173,10 +222,10 @@ const runModuleConfiguration = async (modulesToDeploy) => {
 
 /**
  * Update existing config file
- * @param {string[]} modulesToDeploy
+ * @param {string[]} modules
  * @returns
  */
-const onUpdate = async (modulesToDeploy) => {
+const onUpdate = async (modules) => {
   // load previous config
   appConfig = JSON.parse(
     fs.readFileSync(
@@ -195,7 +244,7 @@ const onUpdate = async (modulesToDeploy) => {
     });
 
   const proceed = await select({
-    message: `It looks like you have previously deployed modules: ${chalk.yellow(modulesToDeploy.join(", "))}. Is that correct?`,
+    message: `It looks like you have previously deployed modules: ${chalk.yellow(previousModules.join(", "))}. Is that correct?`,
     choices: [
       { name: "Yes", value: true },
       { name: "No", value: false },
@@ -207,9 +256,14 @@ const onUpdate = async (modulesToDeploy) => {
   }
 
   // configure newly selected module
-  const newModules = modulesToDeploy.filter((module) => {
+  const newModules = modules.filter((module) => {
     return !previousModules.includes(templateInverseLookup[module]);
   });
+
+  // check if user has deployed AMI module before to Fleet
+  if (modules.includes("workstation-fleet")) {
+    await verifyAmiId();
+  }
 
   await runModuleConfiguration(newModules);
 
@@ -217,19 +271,19 @@ const onUpdate = async (modulesToDeploy) => {
   for (const previous in previousModules) {
     if (
       configurationTemplates[previous] &&
-      !modulesToDeploy.includes(configurationTemplates[previous].name)
+      !modules.includes(configurationTemplates[previous].name)
     ) {
-      modulesToDeploy.push(configurationTemplates[previous].name);
+      modules.push(configurationTemplates[previous].name);
     }
   }
 
   // prompt user with deployment verification
-  if (!(await verifyDeployment(modulesToDeploy))) {
+  if (!(await verifyDeployment(modules))) {
     process.exit(1);
   }
 
   // do the deployment
-  await runDeployment(true, modulesToDeploy);
+  await runDeployment(true, modules);
 };
 
 /**
@@ -251,7 +305,7 @@ const onDeploy = async () => {
     ),
   );
 
-  const modulesToDeploy = await checkbox({
+  const modules = await checkbox({
     message: `Select ${chalk.yellow("one or more")} modules to deploy:`,
     required: true,
     choices: [
@@ -286,7 +340,7 @@ const onDeploy = async () => {
     });
 
     if (updateExisting) {
-      await onUpdate(modulesToDeploy);
+      await onUpdate(modules);
       return 0;
     }
   }
@@ -319,62 +373,204 @@ const onDeploy = async () => {
 
   // set config defaults for all stacks
   for (const [key, value] of Object.entries(configurationTemplates)) {
-    if (modulesToDeploy.includes(key)) continue;
+    if (modules.includes(key)) continue;
     appConfig.stacks[key] = await value["default"]();
   }
 
   appConfig.stacks.vpc = getVpcDefault();
 
-  // TODO: turn goldenAMI prompt into a function
-  if (modulesToDeploy.includes("workstation-fleet")) {
-    const goldenAMIDeployed = await select({
-      message: `You have selected Omniverse Workstation Fleet for deployment. Have you already deployed the Workstation AMI?`,
-      choices: [
-        {
-          name: "Yes",
-          value: true,
-        },
-        {
-          name: "No / Not sure",
-          value: false,
-        },
-      ],
-    });
-
-    if (!goldenAMIDeployed) {
-      console.log(
-        chalk.red(`
-      Before deploying the Omniverse Workstations, it is necessary to deploy the Omniverse Workstations AMI. 
-      Please restart the Deployment Tool and select only the Omniverse Workstation AMI option.
-      The process for deploying the Omniverse Workstation AMI can take several minutes. 
-      For the additional steps post deployment, please refer to the Workstation AMI README: ${getWorkstationsAMIReadme()}
-      `),
-      );
-      process.exit(1);
-    }
+  // check if user has deployed AMI module before to Fleet
+  if (modules.includes("workstation-fleet")) {
+    await verifyAmiId();
   }
 
   // always required
   appConfig.stacks.vpc = await configureVpc();
 
   // run config requirements for each module
-  await runModuleConfiguration(modulesToDeploy);
+  await runModuleConfiguration(modules);
 
   // write new config file to infra.config.json
   writeConfigFile(appConfig);
 
   // prompt user with deployment verification
-  if (!(await verifyDeployment(modulesToDeploy))) {
+  if (!(await verifyDeployment(modules))) {
     process.exit(1);
   }
 
   // do the deployment
-  await runDeployment(cdkConfigured, modulesToDeploy);
+  await runDeployment(cdkConfigured, modules);
 };
 
-// run deployment steps, including module configuration
+/**
+ * Delete command logic
+ * @returns
+ */
+const onDelete = async () => {
+  console.log(
+    chalk.bold(
+      chalk.cyan(
+        `
+      *************************************************************
+        Welcome to the 'NVIDIA Omniverse on AWS' Deployment tool! 
+      *************************************************************
+        
+        We'll guide you through the necessary steps to delete Omniverse modules on your AWS account.
+        `,
+      ),
+    ),
+  );
+
+  // parse existing infra.config.json
+  appConfig = JSON.parse(
+    fs.readFileSync(
+      new URL(
+        path.resolve(__dirname, `../../infra/src/config/infra.config.json`),
+        import.meta.url,
+      ),
+    ),
+  );
+
+  // get list of previously deployed
+  const previousModules = Object.entries(appConfig.stacks)
+    .filter(([_, v]) => v.deployed)
+    .map((v) => {
+      return v[0].includes("vpc") ? "vpc" : configurationTemplates[v[0]].name;
+    });
+
+  // prompt user with deletion verification
+  const proceed = await select({
+    message: `It looks like you have previously deployed modules: ${chalk.yellow(previousModules.join(", "))}. Continue with deleting stacks?`,
+    choices: [
+      { name: "Yes", value: true },
+      { name: "No", value: false },
+    ],
+  });
+
+  if (!proceed) {
+    process.exit(1);
+  }
+
+  // destroy stacks and associated resources
+  execSync(
+    `cdk destroy -c stacks="${previousModules.join(" ")}" --all --verbose`,
+    {
+      cwd: getModuleDirectory(),
+      stdio: "inherit",
+    },
+  );
+
+  // delete retained resources
+  const logsBucketName = "omniverse-aws-nucleus-logs-bucket";
+  console.log(
+    chalk.bold(
+      "Emptying and deleting logs bucket: ",
+      chalk.yellow(logsBucketName),
+    ),
+  );
+
+  try {
+    const s3Client = new S3Client();
+
+    // get logs files in bucket
+    const listObjectsResponse = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: logsBucketName,
+      }),
+    );
+
+    // delete logs and empty bucket
+    const deleteObjectsResponse = await s3Client.send(
+      new DeleteObjectsCommand({
+        Bucket: logsBucketName,
+        Delete: {
+          Objects: listObjectsResponse.Contents.map((v) => ({ Key: v.Key })),
+        },
+      }),
+    );
+
+    console.log(
+      chalk.green(
+        `Deleted Objects: ${JSON.stringify(deleteObjectsResponse.Deleted, null, 2)}\n`,
+      ),
+    );
+
+    // delete bucket
+    const deleteBucketResponse = await s3Client.send(
+      new DeleteBucketCommand({
+        Bucket: logsBucketName,
+      }),
+    );
+
+    console.log(
+      chalk.green(
+        `Bucket Deleted: ${JSON.stringify(deleteBucketResponse, null, 2)}\n`,
+      ),
+    );
+  } catch (error) {
+    console.log(
+      chalk.bold(chalk.red(`Error: ${JSON.stringify(error, null, 2)}\n`)),
+    );
+  }
+
+  try {
+    // describe key pairs
+    const keyNames = ["omni-jumpbox-keypair", "omni-workstation-keypair"];
+    const ec2Client = new EC2Client();
+    const keyPairsResponse = await ec2Client.send(
+      new DescribeKeyPairsCommand({
+        KeyNames: keyNames,
+      }),
+    );
+
+    const keyPairIds = keyPairsResponse.KeyPairs.map((kp) => {
+      return `ec2/keypair/${kp.KeyPairId}`;
+    });
+
+    console.log(
+      chalk.bold(
+        `Deleting Key Pairs: ${JSON.stringify(keyPairIds.join(", "), null, 2)}\n`,
+      ),
+    );
+
+    // delete key pairs
+    for (const key of keyNames) {
+      const deleteKeyResponse = await ec2Client.send(
+        new DeleteKeyPairCommand({
+          KeyName: key,
+        }),
+      );
+
+      console.log(chalk.green(`Key Pair Deleted: ${key}\n`));
+    }
+
+    // delete ssm parameters
+    const ssmClient = new SSMClient();
+
+    const params = [...keyNames, ...keyPairIds];
+    console.log(chalk.bold(`Deleting SSM Parameters: ${params.join(", ")}\n`));
+
+    const deleteParamResponse = await ssmClient.send(
+      new DeleteParametersCommand({ Names: params }),
+    );
+
+    console.log(
+      chalk.green(
+        `Parameters Deleted: ${JSON.stringify(deleteParamResponse.DeletedParameters, null, 2)}\n`,
+      ),
+    );
+  } catch (error) {
+    console.log(
+      chalk.bold(chalk.red(`Error: ${JSON.stringify(error, null, 2)}\n`)),
+    );
+  }
+};
+
+// run tool commands
 if ("deploy" in cli.flags) {
   await onDeploy();
+} else if ("delete" in cli.flags) {
+  await onDelete();
 }
 
 // no errors occurred
